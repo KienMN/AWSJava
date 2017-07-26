08f9f2495d115c87c9f06ad9bb837ca0eb8f05ab
KienMN <kienmn97@gmail.com>
2017-07-26 14:29:28 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by kienmaingoc on 6/27/17.
+ */
+public class Main {
+    public static String ACCESS_KEY_ID = "AKIAITNPIQ3TMODHVGGA";
+    public static String SECRET_ACCESS_KEY = "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";
+    public static String REGION = "eu-west-1";
+
+    public static void main(String[] args) {
+        new FileChooserUI();
+    }
+
+}
