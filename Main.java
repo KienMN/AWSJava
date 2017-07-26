5b5803306a1a748eceb12e4fe401c82f7f92cf62
KienMN <kienmn97@gmail.com>
2017-07-26 14:32:40 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+/**
+ * Created by kienmaingoc on 6/26/17.
+ */
+public class Main {
+
+    public static String AccessKeyID = "AKIAITNPIQ3TMODHVGGA";
+    public static String SecretAccessKey = "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";
+    public static String Region = "eu-west-1";
+    public static String Key = "photo3.jpg";
+    public static String BucketName = "kienimages";
+
+    public static void main(String[] args) {
+        new FileChooserUI();
+    }
+}
