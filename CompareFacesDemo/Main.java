c74eb8af870371c254e594194430d1730037d6dc
KienMN <kienmn97@gmail.com>
2017-07-26 14:31:30 +0700

    Update Main.java

--- CompareFacesDemo/Main.java
+++ CompareFacesDemo/Main.java
@@ -2,8 +2,8 @@
  * Created by kienmaingoc on 6/27/17.
  */
 public class Main {
-    public static String ACCESS_KEY_ID = "AKIAITNPIQ3TMODHVGGA";
-    public static String SECRET_ACCESS_KEY = "qYcQwe6SSJmvV+iqnGvJAxt/8eF0rEbxtEDrr5Ni";
+    public static String ACCESS_KEY_ID = "YOUR_ACCESS_KEY_ID";
+    public static String SECRET_ACCESS_KEY = "YOUR_SECRET_ACCESS_KEY";
     public static String REGION = "eu-west-1";
 
     public static void main(String[] args) {
